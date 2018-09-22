58c8d4cafacc8fb16594d458c896b10d1a8c137e
mental <m3nta1@yahoo.com>
2018-09-22 13:57:33 +0100

    better install script to allow for updates

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-mkdir ~/.config/polybar/gmail
+if [ -e ~/.config/polybar/gmail ]; then
+	echo "Updating gmail-polybar..."
+	cp polybar.py ~/.config/polybar/gmail/
+else
+	mkdir ~/.config/polybar/gmail
+	cp polybar.py ~/.config/polybar/gmail/
 
-cp polybar.py ~/.config/polybar/gmail
+	echo "[module/gmail]
+	type = custom/script
+	exec = python3 ~/.config/polybar/gmail/polybar.py
+	tail = true
+	click-left = xdg-open https://mail.google.com" >> ~/.config/polybar/config
+fi
 
-echo "[module/gmail]
-type = custom/script
-exec = python3 ~/.config/polybar/gmail/polybar.py
-tail = true
-click-left = xdg-open https://mail.google.com" >> ~/.config/polybar/config
