d3e1d909812d22aaca1be18e445f5ec5e60f6838
mental <m3nta1@yahoo.com>
2018-09-22 13:27:45 +0100

    install.sh polybar exec with python3

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cp polybar.py ~/.config/polybar/gmail
 
 echo "[module/gmail]
 type = custom/script
-exec = ~/.config/polybar/gmail/polybar.py
+exec = python3 ~/.config/polybar/gmail/polybar.py
 tail = true
 click-left = xdg-open https://mail.google.com" >> ~/.config/polybar/config
